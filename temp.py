b51bbeb63a543e12c3dd15b89087efaf367862a0
FinanH <fharkness@cree-ate.com>
2025-10-22 11:29:10 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,8 +4,8 @@ import random
 grid = []
 
 def init_grid(): # deffinging the grid vaiable 
-    for i in range(4):
-        row = [0, 0, 0, 0]
+    for i in range(10):
+        row = [0 for i in range(10)]
         grid.append(row)
 
 
@@ -13,16 +13,17 @@ def init_grid(): # deffinging the grid vaiable
 def add_random_num():
     global grid
     zero_cells = []
-    for i in range(4):
-        for j in range(4):
+    for i in range(10):
+        for j in range(10):
             if grid[i][j] == 0:
                 zero_cells.append((i, j))
     if len(zero_cells) > 0:
         random_index = random.randint(0, len(zero_cells) - 1)
         random_cell = zero_cells[random_index]
-        grid[random_cell[0]][random_cell[1]] = 2
+        grid[random_cell[0]][random_cell[1]] = round(random.uniform(0.2, 2), 3)
         
 init_grid()
-add_random_num() 
+for i in range(7):
+    add_random_num() 
 
 print(grid)
