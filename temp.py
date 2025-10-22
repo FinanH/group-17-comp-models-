19847b618bc42c2a2a4572e40116c8a0b8f7426e
FinanH <fharkness@cree-ate.com>
2025-10-22 11:20:52 +0100

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,28 @@
-x = 'hello world'
+#code to generate the stops at a random location on the grid and assining weight values 
+import numpy as np 
+import random
+grid = []
 
-print(x)
+def init_grid(): # deffinging the grid vaiable 
+    for i in range(4):
+        row = [0, 0, 0, 0]
+        grid.append(row)
+
+
+#adding number to a radom palce in the grid
+def add_random_num():
+    global grid
+    zero_cells = []
+    for i in range(4):
+        for j in range(4):
+            if grid[i][j] == 0:
+                zero_cells.append((i, j))
+    if len(zero_cells) > 0:
+        random_index = random.randint(0, len(zero_cells) - 1)
+        random_cell = zero_cells[random_index]
+        grid[random_cell[0]][random_cell[1]] = 2
+        
+init_grid()
+add_random_num() 
+
+print(grid)
